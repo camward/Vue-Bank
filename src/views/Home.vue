d7d2f1c1d46acb393efeb2492ba9cff129301b7e
alex <camward@yandex.ru>
2021-11-14 15:57:39 +0400

    модальные окна. использование teleport

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,30 @@
 <template>
   <app-page title="Список заявок">
     <template #header>
-      <button class="btn primary">Создать</button>
+      <button class="btn primary" @click="modal = true">Создать</button>
     </template>
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false"></app-modal>
+    </teleport>
   </app-page>
 </template>
 
 <script>
+import {ref} from 'vue'
 import AppPage from '../components/ui/AppPage'
+import AppModal from '../components/ui/AppModal'
 
 export default {
   setup() {
+    const modal = ref(false)
+
     return {
+      modal
     }
   },
-  components: {AppPage}
+  components: {
+    AppPage, AppModal
+  }
 }
 </script>
