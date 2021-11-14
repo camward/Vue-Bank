ab0a35c443588a2eff86f2ae338fcc2fd691a979
alex <camward@yandex.ru>
2021-11-14 15:26:16 +0400

    всплывающие окна. отображение ошибок

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,14 +1,16 @@
 <template>
   <the-navbar />
   <div class="container with-nav">
+    <app-message />
     <router-view />
   </div>
 </template>
 
 <script>
 import TheNavbar from '../components/TheNavbar'
+import AppMessage from '../components/ui/AppMessage'
 
 export default {
-  components: {TheNavbar}
+  components: {TheNavbar, AppMessage}
 }
 </script>
--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -25,8 +25,13 @@ export default {
                 const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FB_KEY}`
                 const {data} = await axios.post(url, {...payload, returnSecureToken: true})
                 commit('setToken', data.idToken)
+                commit('clearMessage', null, {root: true})
             } catch (e) {
-                console.log(error(e.response.data.error.message))
+                dispatch('setMessage', {
+                    value: error(e.response.data.error.message),
+                    type: 'danger'
+                }, {root: true})
+                throw new Error()
             }
         }
     },
