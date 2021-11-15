46489c558c7dc1c3ea7d3ffdc23236ed969e34cb
alex <camward@yandex.ru>
2021-11-15 20:45:20 +0400

    загрузка и вывод заявок

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <app-page title="Список заявок">
+  <app-loader v-if="loading" />
+  <app-page title="Список заявок" v-else>
     <template #header>
       <button class="btn primary" @click="modal = true">Создать</button>
     </template>
+
+    <request-table :requests="requests"></request-table>
 
     <teleport to="body">
       <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
@@ -13,21 +16,34 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref, computed, onMounted} from 'vue'
+import {useStore} from 'vuex'
 import AppPage from '../components/ui/AppPage'
+import RequestTable from '../components/request/RequestTable'
 import RequestModal from '../components/request/RequestModal'
 import AppModal from '../components/ui/AppModal'
+import AppLoader from '../components/ui/AppLoader'
 
 export default {
   setup() {
+    const store = useStore()
     const modal = ref(false)
+    const loading = ref(false)
+
+    onMounted(async () => {
+      loading.value = true
+      await store.dispatch('request/load')
+      loading.value = false
+    })
+
+    const requests = computed(() => store.getters['request/requests'])
 
     return {
-      modal
+      modal, requests, loading
     }
   },
   components: {
-    AppPage, AppModal, RequestModal
+    AppPage, RequestTable, AppModal, RequestModal, AppLoader
   }
 }
 </script>
